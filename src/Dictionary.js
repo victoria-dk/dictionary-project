998546e7e5736a5743acbb6dbea54b021d95eeba
Victoria <victoria-dk@hotmail.com>
2021-04-19 00:12:23 +0200

    Add api documentation URL

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -12,6 +12,7 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
 
+    //documentation: https://dictionaryapi.dev/
     let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiURL).then(handleResponse);
   }
